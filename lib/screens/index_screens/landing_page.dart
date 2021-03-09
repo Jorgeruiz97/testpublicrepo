4b730082bd8935a1b1af242d158210e252d64c28
Parabeac <ivanh@parabeac.com>
2021-03-09 16:18:34 +0000

    undefined

---
+++ lib/screens/index_screens/landing_page.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+
+class LandingPage extends StatefulWidget{
+  const LandingPage() : super();
+  @override
+  _LandingPage createState() => _LandingPage();
+}
+
+class _LandingPage extends State<LandingPage>{
+  
+  _LandingPage();
+
+  @override
+  Widget build(BuildContext context){
+    
+    return Scaffold(
+backgroundColor: Colors.white,appBar: AppBar(leading:     IconButton(
+        icon: Image.asset('assets/images/0_117.png', width: 25.000,height: 30.000,),
+        onPressed: () {
+            // TODO: Fill action
+            },
+            ),
+    title: Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+    children:[
+      Flexible(flex: 58,child: Container(width: 40.000,height: 27.000,child: Align(alignment: Alignment(0.00, 0.00),child: AutoSizeText(
+'285',
+style: TextStyle(
+fontFamily: 'Sanchez',
+fontSize: 21.0,
+fontWeight: FontWeight.w400,
+fontStyle: FontStyle.normal,
+letterSpacing: 0.0,
+color: Colors.white,),textAlign: TextAlign.left,
+
+),)),),Spacer(flex: 12,),Flexible(flex: 32,child: Image.asset('assets/images/0_116.png', width: 22.000,height: 22.000,),)
+    ]
+    )
+    ,actions: [    IconButton(
+        icon: Image.asset('assets/images/0_118.png', width: 34.000,height: 34.000,),
+        onPressed: () {
+            // TODO: Fill action
+            },
+            ),
+    ],),
+body: Align(alignment: Alignment(0.00, 1.00),child: Container(width: MediaQuery.of(context).size.width * 1.000,height: MediaQuery.of(context).size.height * 0.869,child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+children: [Flexible(flex: 59,child: Padding(padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.21,right: MediaQuery.of(context).size.width * 0.20,),child: Image.asset('assets/images/0_101.png', width: MediaQuery.of(context).size.width * 0.589,height: MediaQuery.of(context).size.height * 0.509,)),),Spacer(flex: 4,),Flexible(flex: 39,child: Padding(padding: EdgeInsets.only(),child: Container(width: MediaQuery.of(context).size.width * 1.000,height: MediaQuery.of(context).size.height * 0.334,decoration: BoxDecoration(color: Colors.white,),child: Align(alignment: Alignment(-0.01, -0.34),child: Container(width: MediaQuery.of(context).size.width * 0.779,height: MediaQuery.of(context).size.height * 0.251,child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+children: [Flexible(flex: 10,child: Padding(padding: EdgeInsets.only(),child: Container(width: MediaQuery.of(context).size.width * 0.779,height: MediaQuery.of(context).size.height * 0.025,child: Align(alignment: Alignment(0.00, 0.00),child: AutoSizeText(
+'A new way to learn about careers',
+style: TextStyle(
+fontFamily: 'Sanchez',
+fontSize: 16.0,
+fontWeight: FontWeight.w400,
+fontStyle: FontStyle.normal,
+letterSpacing: 0.2222222238779068,
+color: Color(0xff042c5c),),textAlign: TextAlign.center,
+
+),))),),Spacer(flex: 5,),Flexible(flex: 20,child: Padding(padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.08,right: MediaQuery.of(context).size.width * 0.07,),child: Container(width: MediaQuery.of(context).size.width * 0.627,height: MediaQuery.of(context).size.height * 0.049,child: Align(alignment: Alignment(0.00, 0.00),child: AutoSizeText(
+'Inspyred provides resources to help you make the best decisions.',
+style: TextStyle(
+fontFamily: 'Sanchez',
+fontSize: 13.0,
+fontWeight: FontWeight.w400,
+fontStyle: FontStyle.normal,
+letterSpacing: 0.11100000143051147,
+color: Color(0xff77869e),),textAlign: TextAlign.center,
+
+),))),),Spacer(flex: 16,),Flexible(flex: 21,child: Padding(padding: EdgeInsets.only(),child: Container(width: MediaQuery.of(context).size.width * 0.768,height: MediaQuery.of(context).size.height * 0.052,decoration: BoxDecoration(color: Color(0xff3598fb),
+borderRadius: BorderRadius.all(Radius.circular(100.0)),border: Border.all(color: Color(0xff979797),),),child: Align(alignment: Alignment(0.00, 11.00),child: Container(width: MediaQuery.of(context).size.width * 0.176,height: MediaQuery.of(context).size.height * 0.049,child: Align(alignment: Alignment(0.00, 0.00),child: AutoSizeText(
+'Sign Up',
+style: TextStyle(
+fontFamily: 'Sanchez',
+fontSize: 16.0,
+fontWeight: FontWeight.w400,
+fontStyle: FontStyle.normal,
+letterSpacing: 0.0,
+color: Colors.white,),textAlign: TextAlign.center,
+
+),)),))),),Spacer(flex: 10,),Flexible(flex: 21,child: Padding(padding: EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.01,),child: Container(width: MediaQuery.of(context).size.width * 0.768,height: MediaQuery.of(context).size.height * 0.052,decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(100.0)),border: Border.all(color: Color(0xff979797),),),child: Align(alignment: Alignment(0.00, 11.00),child: Container(width: MediaQuery.of(context).size.width * 0.147,height: MediaQuery.of(context).size.height * 0.049,child: Align(alignment: Alignment(0.00, 0.00),child: AutoSizeText(
+'Log in',
+style: TextStyle(
+fontFamily: 'Sanchez',
+fontSize: 16.0,
+fontWeight: FontWeight.w400,
+fontStyle: FontStyle.normal,
+letterSpacing: 0.0,
+color: Color(0xff7e7e7e),),textAlign: TextAlign.center,
+
+),)),))),),])),))),),])),), 
+);
+  }
+
+      @override
+    void dispose() {
+      
+      super.dispose();
+    }
+    
+}
+
---
+++ lib/view/small_major_card/small_major_card_default.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+
+class SmallMajorCardDefault extends StatelessWidget{
+  final constraints;
+  
+  const SmallMajorCardDefault(this.constraints, {Key key, }) : super(key : key);
+  
+
+  @override
+  Widget build(BuildContext context){
+    return Container(width: 165.000,height: 80.000,child: Stack(
+children: [Container(width: 165.000,height: 80.000,decoration: BoxDecoration(color: Color(0xff3657c4),
+borderRadius: BorderRadius.all(Radius.circular(10.0)),border: Border.all(color: Color(0xff979797),),),),Positioned(right: 7.0,top :8.0,child: Container(width: 32.000,height: 32.000,child: Stack(
+children: [Image.asset('assets/images/41_123.png', width: 32.000,height: 32.000,),Positioned(left: 6.0,top :6.0,child: Image.asset('assets/images/41_158.png', width: 16.667,height: 15.292,),),])),),Positioned(left: 12.0,top :13.0,child: Container(width: 117.000,height: 44.000,child: AutoSizeText(
+'Information Systems',
+style: TextStyle(
+fontFamily: 'Sanchez',
+fontSize: 17.0,
+fontWeight: FontWeight.w400,
+fontStyle: FontStyle.normal,
+letterSpacing: 0.0,
+color: Colors.white,),textAlign: TextAlign.left,
+
+)),),]));
+  }
+}
