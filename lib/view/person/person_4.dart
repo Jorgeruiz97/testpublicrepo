14f5640d526085194dcc5356a40604b562a76ecf
Parabeac <ivanh@parabeac.com>
2021-03-18 04:56:50 +0000

    undefined

---
+++ lib/view/person/person_4.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Person4 extends StatelessWidget {
+  final constraints;
+
+  const Person4(
+    this.constraints, {
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'assets/images/0_21.png',
+      width: 41.000,
+      height: 41.000,
+    );
+  }
+}
