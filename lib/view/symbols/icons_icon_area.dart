d1015a6e16b97046e4a054ec19f808e3003e01b3
Parabeac <ivanh@parabeac.com>
2021-03-20 01:39:44 +0000

    undefined

---
+++ lib/view/symbols/icons_icon_area.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class IconsIconArea extends StatelessWidget {
+  final constraints;
+
+  const IconsIconArea(
+    this.constraints, {
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'assets/images/0_47.png',
+      width: 24.000,
+      height: 24.000,
+    );
+  }
+}
