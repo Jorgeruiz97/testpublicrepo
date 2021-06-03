d5d21960c1b1bd16945375613acb441301b8f8ae
Parabeac-Nest <admin@parabeac.com>
2021-06-03 14:15:19 +0000

    Auto-generated commit by Parabeac Nest

---
+++ android/app/src/main/kotlin/com/example/zoesproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zoesproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:provider/provider.dart';
+import 'models/small_major_card.dart';
+import 'models/person.dart';
+import 'package:flutter/material.dart';
+import 'screens/index_screens/landing_page.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => SmallMajorCard()),
+        ChangeNotifierProvider(create: (_) => Person())
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'zoesproject',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LandingPage(),
+    );
+  }
+}
